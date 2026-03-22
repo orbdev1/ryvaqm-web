e12dbf35521855bb42c2f7e8170e8b84adbbb5ea
orbdev1 <ing.ryqm@gmail.com>
2026-03-22 16:11:29 -0500

    update: add cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,10 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
+  experimental: {
+    queuedRendering: {
+      enabled: true,
+    },
+  },
   integrations: [react()]
 });
